e971be2561ee036c4173d315abca01e5ebcb6132
Ravi Kalla <ravi2523096@gmail.com>
2020-08-15 14:45:05 -0400

    first commit

---
+++ src/main/java/in/ravikalla/springBootDemo/controller/DemoWebservice.java
@@ -0,0 +1,23 @@
+package in.ravikalla.springBootDemo.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// http://localhost:8080/demoWebservice/name
+
+@RestController
+@RequestMapping("/demoWebservice")
+public class DemoWebservice {
+
+	private static final Logger L = LogManager.getLogger(DemoWebservice.class);
+
+	@RequestMapping(value="/name", method=RequestMethod.GET)
+	public String getName() {
+		L.info("Start : DemoWebservice.getName()");
+		return "Ravi Demo";
+	}
+}
---
+++ src/test/java/in/ravikalla/springBootDemo/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ravikalla.springBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
